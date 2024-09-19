647150e9c385b1cdd78ac8d49f42e4d621b7a736
DepyXa <87533465+DepyXa@users.noreply.github.com>
2024-09-19 18:36:04 +0300

    Update __init__.py

--- Flastel/__init__.py
+++ Flastel/__init__.py
@@ -1,4 +1,5 @@
-from .func.polling import TelegramPollingBot
+from .func.polling_func import TelegramPollingBot, polling_func
+from .func.webhook_func import webhook_func
 from .func.methods import TelegramBot
 
-__all__ = ["TelegramPollingBot", "TelegramBot"]
+__all__ = ["polling_func", "webhook_func", "TelegramBot", "TelegramPollingBot"]
