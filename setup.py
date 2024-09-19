ddb584218846520231e9b1970ac5aac6680a6d55
DepyXa <87533465+DepyXa@users.noreply.github.com>
2024-09-19 15:49:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+setup(
+  name='Flatel',
+  version='0.0.1',
+  author='DepyXa',
+  author_email='dere96632@gmail.com',
+  description='Flastel – Telegram Bot API.',
+  long_description=readme(),
+  long_description_content_type='text/html',
+  url='https://github.com/DepyXa/Flastel',
+  packages=find_packages(),
+  install_requires=[
+    'requests>=2.25.1'
+  ],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='Telegram Bot API ',
+  project_urls={
+    'GitHub': 'https://github.com/DepyXa'
+  },
+  python_requires='>=3.6'
+)
